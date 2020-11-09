8ef2c649afb5dc92249bebb032f8b3f6401a3d79
AndresZ <andry_001@hotmail.com>
2020-11-08 20:14:01 -0500

    iss01

---
+++ flappyBird.js
@@ -0,0 +1,10 @@
+var cvs = document.getElementById("canvas");
+var ctx = cvs.getContext("2d");
+
+// load images
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
