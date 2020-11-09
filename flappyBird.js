ce2db69b831df4328743dbf63a2a49ab933249b1
AndresZ <andry_001@hotmail.com>
2020-11-08 20:18:58 -0500

    rutas js imagenes src iss03

--- flappyBird.js
+++ flappyBird.js
@@ -8,3 +8,9 @@ var bg = new Image();
 var fg = new Image();
 var pipeNorth = new Image();
 var pipeSouth = new Image();
+
+bird.src = "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
